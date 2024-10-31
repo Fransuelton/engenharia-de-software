044ddbec9e08a6d312c76c5f5fd377d5d738987e
Fransuelton <elton6103@gmail.com>
2024-10-31 19:54:45 -0300

    ✨ feat: add PHP examples demonstrating integration with HTML

---
+++ desenv-web-em-html-css-javascript-php/tema-2-o-ambiente-web-cliente-x-servidor-e-a-as-tecnologias/4-modulo-tecnologias-do-lado-servidor/php-html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Código PHP mesclado com código HTML</title>
+</head>
+
+<body>
+    <p>
+        <?php echo "Este é um texto de parágrafo escrito utilizando PHP"; ?>
+    </p>
+</body>
+
+</html>
