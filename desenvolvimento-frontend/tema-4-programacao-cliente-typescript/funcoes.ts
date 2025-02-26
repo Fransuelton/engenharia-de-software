04fa8155793701bbb2f4a35a44e1e09abb7f455a
elton <elton6103@gmail.com>
2025-02-26 13:48:14 -0300

    ✨ feat: add functions for summing elements of an array and calculating factorial in TypeScript

--- desenvolvimento-frontend/tema-4-programacao-cliente-typescript/funcoes.ts
+++ desenvolvimento-frontend/tema-4-programacao-cliente-typescript/funcoes.ts
@@ -31,3 +31,30 @@ let t1: string = "primeiro";
 let t2: string = "segundo";
 let texto_concat = concatenar_texto(t1, t2);
 console.log(`A concatenação de ${t1} com ${t2} é: ${texto_concat}`);
+
+// Função para manipular vetores
+
+function somatorio(pvetor: number[]): number {
+  let soma: number = 0;
+  for (let val of pvetor) {
+    soma += val;
+  }
+  return soma;
+}
+
+let vetor: number[] = [1, 2, 3, 4, 5];
+let res_soma: number = somatorio(vetor);
+console.log(`O somatório dos elementos do vetor ${vetor} é: ${res_soma}`);
+
+// Funções recursivas
+
+function fatorial(n: number): number {
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  return n * fatorial(n - 1);
+}
+
+let numero: number = 5;
+let res_fat = fatorial(numero);
+console.log(`O fatorial de ${numero} é: ${res_fat}`);
