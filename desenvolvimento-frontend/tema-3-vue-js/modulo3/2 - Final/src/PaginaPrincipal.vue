ec59d43e79c2a5218e014df679342bfe4d287296
elton <elton6103@gmail.com>
2025-02-26 05:27:47 -0300

    ✨ feat: add initial Vue 3 application setup with Vite, including main files, components, and styles

---
+++ desenvolvimento-frontend/tema-3-vue-js/modulo3/2 - Final/src/PaginaPrincipal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Cabecalho>
+      <template v-slot:subtitulo>
+        <h2>Subtítulo aqui</h2>
+      </template>
+
+      <template v-slot:extra>
+        <h3>Texto extra</h3>
+      </template>
+    </Cabecalho>
+
+    <ListaCidades />
+  </div>
+</template>
+
+<script>
+import "./assets/estilo.css";
+import Cabecalho from "./components/Cabecalho.vue";
+import ListaCidades from "./components/ListaCidades.vue";
+
+export default {
+  components: { Cabecalho, ListaCidades },
+
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style></style>
