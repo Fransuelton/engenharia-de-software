b4b01ea45b93a188d8f0131e1331be3d5fd7becc
elton <elton6103@gmail.com>
2025-02-26 05:26:56 -0300

    ✨ feat: add Vue.js components for weather application including header, city card, city list, and user profile

---
+++ desenvolvimento-frontend/tema-3-vue-js/modulo3/2 - Final/src/components/Cabecalho.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>App Clima</h1>
+
+    <slot name="subtitulo"></slot>
+    <slot name="extra"></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
