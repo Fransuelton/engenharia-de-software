8918059da723b94f8906f13305dfbf8cbd473867
Fransuelton <elton6103@gmail.com>
2025-07-15 10:56:09 -0300

    ✨ feat: implement Main class with variable declaration examples

---
+++ poo-java/curso-java/sintaxe-basica/01-variaveis/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Sintaxe de declaração de variável: t
+
+        int minhaIdade = 20; // Declarando variável do tipo int
+        String meuNome = "Elton"; // Declarando variável do tipo string
+
+        // Declarando variável utilizando var (aqui o próprio java vai dizer o tipo da variável baseado no valor passado)
+        var estudeiHoje = true;
+    }
+}
