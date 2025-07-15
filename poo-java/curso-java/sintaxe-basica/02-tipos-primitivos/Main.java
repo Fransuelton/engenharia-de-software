18649f080847267a4aa793068886f695806c5a5c
Fransuelton <elton6103@gmail.com>
2025-07-15 10:56:16 -0300

    ✨ feat: add Main class with examples of primitive data types

---
+++ poo-java/curso-java/sintaxe-basica/02-tipos-primitivos/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    // numeros inteiros = 100, 234544, 255, 404, 1. Existem vários tipos de inteiros
+
+    // byte => 8 bits => -128 a 127 (Economiza espaço na memória)
+    // short => 16 bits => -32.768 a 32.767
+    // int => 32 bits => -2.147.483.648 a 2.147.483.647 (mais comum)
+    // long => 64 bits => -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+    // -----
+
+    // numeros decimais = 1.0, 2.5, 3.14, 0.99, -10.5
+
+    // float => 32 bits => (precisão simples)
+    // double => 64 bits => (precisão dupla)
+
+    // -----
+
+    // String usa aspas duplas e char usa aspas simples
+    // String é uma classe em Java, enquanto char é um tipo primitivo
+
+    // String => representar palavras e frases => "Olá, mundo!", "Java é divertido", "12345"
+    // char => representar um único caractere (letra, número, símbolo) => a, b, c, d
+
+    // -----
+
+    // boolean => verdadeiro ou falso (true ou false)
+
+    
+}
