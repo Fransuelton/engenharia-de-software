3e59f9d79ddc84e4e6cfd46a4acdf04efb25f277
Fransuelton <elton6103@gmail.com>
2025-07-15 15:27:34 -0300

    ✨ feat: implement Sandero class with acelerar, freiar, and parar methods

---
+++ poo-java/curso-java/poo/03-interfaces-e-classes-abstratas/Sandero.java
@@ -0,0 +1,21 @@
+
+
+public class Sandero implements Carro {
+
+    @Override
+    public void acelerar() {
+        System.out.println("Acerelando o Sandero");
+    }
+
+    @Override
+    public void freiar() {
+        System.out.println();
+    }
+
+    @Override
+    public void parar() {
+        System.out.println();
+    }
+
+
+}
