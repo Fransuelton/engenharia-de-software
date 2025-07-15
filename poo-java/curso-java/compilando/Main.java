384d75f838e3f8c10305b85052bb3e526a1548f4
Fransuelton <elton6103@gmail.com>
2025-07-15 10:56:00 -0300

    ✨ feat: add Main class for Java project with compilation instructions

---
+++ poo-java/curso-java/compilando/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    // Compilando projeto JAVA pelo terminal
+
+    // 1. Abrir o terminal na pasta do projeto
+
+    // Para gerar o executável do java usamos javac e vamos passar como parâmetro o
+    // nome do programa java (nome da classe)
+    // Todos os arquivos que o .java usar vão ser compilados e linkados por conta do
+    // arquivo de entrada
+
+    // Esse comando vai gerar um arquivo .class que seria o bytecode e basta acessar
+    // o comando Java Main ou outro nome do arquivo.
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
