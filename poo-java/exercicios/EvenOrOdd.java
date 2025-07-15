69a9c92ef2155c15eb837c85068219f5444d0b22
Fransuelton <elton6103@gmail.com>
2025-07-15 18:05:35 -0300

    ✨ feat: add EvenOrOdd class to determine and display if a number is even or odd

---
+++ poo-java/exercicios/EvenOrOdd.java
@@ -0,0 +1,11 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int number = 12;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " Is Even");
+        } else {
+            System.out.println(number + " Is Odd");
+        }
+    }
+}
